2b4c45369a6edbcd0204fa1b7e438aa082809827
Archerolk <99245999+Archerolk@users.noreply.github.com>
2022-02-10 12:24:38 +0200

    Add files via upload

---
+++ frontend/controllers/SiteController.php
@@ -0,0 +1,33 @@
+<?php
+namespace frontend\controllers;
+
+use Yii;
+use yii\base\InvalidArgumentException;
+use yii\web\BadRequestHttpException;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use yii\filters\AccessControl;
+
+
+class SiteController extends Controller
+{
+  
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+    
+}
